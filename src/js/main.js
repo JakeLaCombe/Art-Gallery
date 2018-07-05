3743c8646397536aa0d077eba7d398c0f920e8a6
JakeLaCombe <jake.lacombe2@gmail.com>
2018-07-05 14:14:39 -0500

    Setup Gallery Floor

--- src/js/main.js
+++ src/js/main.js
@@ -7,20 +7,55 @@ class Main extends AbstractApplication {
   constructor () {
     super()
 
-    // const texture = new THREE.TextureLoader().load('static/textures/crate.gif')
+    var axes = new THREE.AxisHelper( 20 );
+    this._scene.add(axes);
 
-    const geometry = new THREE.BoxGeometry(200, 200, 200)
-    // const material = new THREE.MeshBasicMaterial({ map: texture })
+    var planeGeometry = new THREE.PlaneGeometry(150, 150, 1, 1);
+    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xB27752, side: THREE.DoubleSide});
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
-    const material2 = new THREE.ShaderMaterial({
-      vertexShader: shaderVert,
-      fragmentShader: shaderFrag
-    })
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15
+    plane.position.y = 0
+    plane.position.z = 0
 
-    this._mesh = new THREE.Mesh(geometry, material2)
-    this._scene.add(this._mesh)
+    this._scene.add(plane);
 
+    this.setupPlanes();
+
+    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
+    var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true, side: THREE.DoubleSide});
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+    cube.position.x = -4;
+    cube.position.y = 3;
+    cube.position.z = 0;
+
+    this._scene.add(cube);
+
+    var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+    var sphereMaterial = new THREE.MeshBasicMaterial({color: 0x7777ff, wireframe: true});
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+    sphere.position.x = 20;
+    sphere.position.y = 4;
+    sphere.position.z = 2;
+
+    this._scene.add(sphere);
     this.animate()
+  }
+
+  setupPlanes() {
+    var planeGeometry = new THREE.PlaneGeometry(30, 20, 0, 0);
+    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc, side: THREE.DoubleSide});
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+    plane.rotation.x = 0;
+    plane.position.x = 15;
+    plane.position.y = 10;
+    plane.position.z = 0
+
+    this._scene.add(plane);
   }
 }
 
